9aa3728ef9ae397b3d14bc285c24b58e77721968
Shon <hsuizip@gmail.com>
2018-07-25 10:07:05 +0800

    加入Interface IRepository 新增兩個方法

---
+++ WebApplicationTest/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplicationTest.Models;
+
+namespace WebApplicationTest.Repository
+{
+    public interface IRepository
+    {
+        List<Product> GetAllProduct();
+        Product GetProductById(int Id);
+    }
+}
