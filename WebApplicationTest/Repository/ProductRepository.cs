011f9261bd1a70d829de01b9aa9b924579cecc61
Shon <hsuizip@gmail.com>
2018-07-25 13:36:52 +0800

    實作IRepository

---
+++ WebApplicationTest/Repository/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplicationTest.Models;
+
+namespace WebApplicationTest.Repository
+{
+    public class ProductRepository : IRepository
+    {
+        public List<Product> GetAllProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
