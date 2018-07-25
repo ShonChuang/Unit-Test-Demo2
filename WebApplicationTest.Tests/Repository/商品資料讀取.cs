c7e9a0f28c61bea94aefe3e6981a94b1ea03432d
Shon <hsuizip@gmail.com>
2018-07-25 10:15:20 +0800

    商品單元測試建立

---
+++ WebApplicationTest.Tests/Repository/商品資料讀取.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApplicationTest.Tests.Repository
+{
+    [TestClass]
+    public class 商品資料讀取
+    {
+        [TestMethod]
+        public void 首頁_商品_讀取全部資料()
+        {
+             
+        }
+        [TestMethod]
+        public void 首頁_商品_讀取單筆資料()
+        {
+
+        }
+    }
+}
