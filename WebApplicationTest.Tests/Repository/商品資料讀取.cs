344f53c000efb3759ababa84d6d7b9ab91ffdd0a
Shon <hsuizip@gmail.com>
2018-07-25 10:32:30 +0800

    測試讀取所有商品Method

--- WebApplicationTest.Tests/Repository/商品資料讀取.cs
+++ WebApplicationTest.Tests/Repository/商品資料讀取.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using WebApplicationTest.Models;
+using WebApplicationTest.Repository;
 
 namespace WebApplicationTest.Tests.Repository
 {
@@ -9,7 +13,19 @@ namespace WebApplicationTest.Tests.Repository
         [TestMethod]
         public void 首頁_商品_讀取全部資料()
         {
-             
+            //Arrange
+            var ProductRepository = Substitute.For<IRepository>();
+            List<Product> ProductList = new List<Product>()
+            {
+                 new Product{ Id=1, Name="商品1", IsEnable=true, Price=100 },
+                 new Product{ Id=1, Name="商品2", IsEnable=true, Price=100 },
+                 new Product{ Id=1, Name="商品3", IsEnable=true, Price=100 }
+            };
+            ProductRepository.GetAllProduct().Returns(ProductList);
+            //Act
+            var Result = ProductRepository.GetAllProduct();
+            //Assert
+            Assert.AreEqual(ProductList, Result);
         }
         [TestMethod]
         public void 首頁_商品_讀取單筆資料()
