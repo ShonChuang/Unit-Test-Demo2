d1d98eab16e6bffa4b75b5b42253aa65ba80359d
Shon <hsuizip@gmail.com>
2018-07-25 10:01:19 +0800

    加入 Product Model

---
+++ WebApplicationTest/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationTest.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public bool IsEnable { get; set; }
+    }
+}
