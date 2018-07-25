bfcd717fab9e2b7bcbd32df8a0d6688fe5023fdb
Shon <hsuizip@gmail.com>
2018-07-25 13:39:01 +0800

    將Repository 注入HomeController 產生新的Actrion

--- WebApplicationTest/Controllers/HomeController.cs
+++ WebApplicationTest/Controllers/HomeController.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplicationTest.Repository;
 
 namespace WebApplicationTest.Controllers
 {
     public class HomeController : Controller
     {
+        private IRepository _repository;
+        public HomeController(IRepository repository)
+        {
+            _repository = repository;
+        }
+        public HomeController()
+        {
+            this._repository = new ProductRepository();
+        }
+        public ActionResult GetProduct()
+        {
+           var Product =  this._repository.GetAllProduct();
+            return View(Product);
+        }
+
         public ActionResult Index()
         {
             return View();
